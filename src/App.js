a03d067a343e711b13757ecd4ac91b85e6175b4f
mykenzie <mykenzierogers@gmail.com>
2019-06-10 13:22:14 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
+import './reset.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      menuStatus: 'drop-down-menu'
+    }
+  }
+
+  handleClick = () => {
+    if(this.state.menuStatus === 'drop-down-menu-open') {
+      this.setState({menuStatus: 'drop-down-menu-closed'});
+    } else {
+      this.setState({menuStatus: 'drop-down-menu-open'});
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <ul>
+          <div>
+            <li>Start Bootstrap</li>
+          </div>
+          <li className='menu-text'>Home</li>
+          <li className='menu-text'>About</li>
+          <li className='menu-text'>Services</li>
+          <li className='menu-text'>Contact</li>
+          <li>
+            <img 
+            onClick={this.handleClick}
+            src="https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/menu-alt-512.png" 
+            alt="ham_menu" />
+          </li>
+          <div className={this.state.menuStatus}>
+
+          </div>
+        </ul>
+      </nav>
+    )
+  }
+};
+
 
 export default App;
